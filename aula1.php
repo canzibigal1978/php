6821711b6e187ab85d863f6d23bd8bd9f64b4fa9
bigalsoft informatica <bigalsoft.info@gmail.com~>
2024-02-29 21:50:55 -0300

    Curso de PHP #02 - Constantes e Variáveis

--- aula1.php
+++ aula1.php
@@ -1,25 +1,23 @@
 <?php
+// comentario simples
+/*
+  comentario em varias linha
+  linha 2
+  linha 3
+  linha 4
+ */
 
-    // comentario simples
-    /*
-        comentario em varias linha
-        linha 2
-        linha 3
-        linha 4
-    */
-
-    echo '<font color=#ff0000>Primeira aula php</font>';
-    echo '<br/>Curso do PHP';
-
+echo '<font color=#ff0000>Primeira aula php</font>';
+echo '<br/>Curso do PHP';
 ?>
 <html>
     <head>
-    <title>Aula 1 de PHP</title>
-</head>
-<body>
-    <p>Texto escrito em html</p>
-    <?php
+        <title>Aula 1 de PHP</title>
+    </head>
+    <body>
+        <p>Texto escrito em html</p>
+        <?php
         echo 'Texto escrito em php';
-    ?>
-</body>
+        ?>
+    </body>
 </html>
--- aula2.php
+++ aula2.php
@@ -1,33 +1,32 @@
 <?php
 //variaveis
 $vnome = "Francisco";
-$vnum=10;
-$vnum2=20.5;
-$soma=0;
+$vnum = 10;
+$vnum2 = 20.5;
+$soma = 0;
 
 //constante
 define("cnome", "Bigalsoft");
 define("ver", PHP_VERSION);
 define("dir", __DIR__);
-        
-        
+
 echo "Nome: $vnome <br/>";
-$vnome="Bigal";
+$vnome = "Bigal";
 echo "Nome: $vnome <br/>";
-$vnome="Fran ";
-echo "Nome: ".$vnome."<br/>";
+$vnome = "Fran ";
+echo "Nome: " . $vnome . "<br/>";
 echo "Num1: $vnum <br/>";
 echo "Num2: $vnum2 <br/>";
-$soma=$vnum+$vnum2;
+$soma = $vnum + $vnum2;
 echo "Soma: $soma <br/>";
 
 echo "<hr/>";
-echo "Constante cnome: ".cnome."<br/>"; 
-echo "Nome do arquivo: ".__FILE__."<br/>";
-echo "Versão PHP: ".ver."<br/>";
-echo "Pasta: ".dir."<br/>";
-echo "Versão do SO: ".PHP_OS."<br/>";
-echo "linha: ".__LINE__."<br/>";
+echo "Constante cnome: " . cnome . "<br/>";
+echo "Nome do arquivo: " . __FILE__ . "<br/>";
+echo "Versão PHP: " . ver . "<br/>";
+echo "Pasta: " . dir . "<br/>";
+echo "Versão do SO: " . PHP_OS . "<br/>";
+echo "linha: " . __LINE__ . "<br/>";
 ?>
 <html>
     <head>
@@ -35,6 +34,6 @@ echo "linha: ".__LINE__."<br/>";
         <title>Aula 2 de PHP - constantes e variaveis</title>
     </head>
     <body>
-        
+
     </body>
 </html>
