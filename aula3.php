797bfcbf34b6279e274d331e47bad62641a3cdd9
bigalsoft informatica <bigalsoft.info@gmail.com~>
2024-03-01 20:16:16 -0300

    Curso de PHP #03 - Operações com Variáveis

---
+++ aula3.php
@@ -0,0 +1,64 @@
+<?php
+//variaveis
+$num1=10;
+$num2=5;
+
+//aritmeticos operadores
+
+$res = $num1+$num2;
+echo "Soma de $num1 com $num2 = $res <br/>";
+
+$res2 = $num1-$num2;
+echo "Sub de $num1 com $num2 = $res2 <br/>";
+
+$res3 = $num1*$num2;
+echo "Multi de $num1 com $num2 = $res3 <br/>";
+
+$res4 = $num1/$num2;
+echo "Div de $num1 com $num2 = $res4 <br/>";
+
+$res5 = $num1%$num2;
+echo "Resto de $num1 com $num2 = $res5 <br/>";
+
+
+//operadores de atribuição
+echo "<hr/>";
+
+// $num1=$num1+1;
+$num1++;
+echo "Novo valor de num1 e $num1 <br/>";
+
+// $num1=$num1+1;
+$num2+= 2;
+echo "Novo valor de num2 e $num2 <br/>";
+
+$num2*= 12;
+echo "Novo valor de num2 e $num2 <br/>";
+
+$num2/= 22;
+echo "Novo valor de num2 e $num2 <br/>";
+
+
+/*
+ * $num1 == $num2
+ * $num1 != $num2
+ * $num1 <> $num2
+ * $num1 > $num2
+ * $num1 < $num2
+ * $num1 >= $num2
+ * $num1 <= $num2
+ * 
+ * AND &&
+ * OR ||
+*/
+
+?>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <title>Curso de PHP #03 - Operações com Variáveis</title>
+    </head>
+    <body>
+
+    </body>
+</html>
